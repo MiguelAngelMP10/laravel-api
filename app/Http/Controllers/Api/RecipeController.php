ddf5a7a308cef50416b769536d8442ff9083d6a6
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-10-31 19:29:27 -0600

    Recursos anidados o multinivel

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -3,21 +3,27 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\RecipeResource;
 use App\Models\Recipe;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class RecipeController extends Controller
 {
-    public function index(): Collection
+    public function index(): AnonymousResourceCollection
     {
         // all, get
-        return Recipe::with('category', 'tags', 'user')->get();
+        $recipes = Recipe::with('category', 'tags', 'user')->get();
+
+        return RecipeResource::collection($recipes);
     }
 
-    public function show(Recipe $recipe): Recipe
+    public function show(Recipe $recipe): RecipeResource
     {
-        return $recipe->load('category', 'tags', 'user');
+        $recipe = $recipe->load('category', 'tags', 'user');
+
+        return new RecipeResource($recipe);
     }
 
     public function store()
--- app/Http/Controllers/Api/TagController.php
+++ app/Http/Controllers/Api/TagController.php
@@ -3,20 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TagResource;
 use App\Models\Recipe;
 use App\Models\Tag;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class TagController extends Controller
 {
-    public function index(): Collection
+    public function index(): AnonymousResourceCollection
     {
-        return Tag::with('recipes')->get();
+        return TagResource::collection(Tag::with('recipes')->get());
     }
 
-    public function show(Tag $tag): Tag
+    public function show(Tag $tag): TagResource
     {
-        return $tag->load('recipes');
+        return new TagResource($tag->load('recipes'));
     }
 }
