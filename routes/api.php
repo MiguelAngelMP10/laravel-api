19c173c940b38af3fd07369e19f6e3ae6e6f5495
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-10-31 22:04:10 -0600

    Autorización: tokens y middlewares

--- routes/api.php
+++ routes/api.php
@@ -21,11 +21,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('categories', [CategoryController::class, 'index']);
-Route::get('categories/{category}', [CategoryController::class, 'show']);
-
-Route::apiResource('recipes', RecipeController::class);
-
-
-Route::get('tags', [TagController::class, 'index']);
-Route::get('tags/{tag}', [TagController::class, 'show']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('categories', [CategoryController::class, 'index']);
+    Route::get('categories/{category}', [CategoryController::class, 'show']);
+    Route::apiResource('recipes', RecipeController::class);
+    Route::get('tags', [TagController::class, 'index']);
+    Route::get('tags/{tag}', [TagController::class, 'show']);
+});
