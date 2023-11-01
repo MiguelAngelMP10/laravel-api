c790835c1e0f741b9ef86ba358bd4ee37b4be2af
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-10-31 19:23:41 -0600

    Consistencia entre endpoints: recursos y colecciones

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,19 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryCollection;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use App\Http\Resources\CategoryResource;
+
 
 class CategoryController extends Controller
 {
-    public function index(): Collection
+    public function index(): CategoryCollection
     {
-        return Category::all();
+        return new CategoryCollection(Category::all());
     }
 
-    public function show(Category $category): Category
+    public function show(Category $category): CategoryResource
     {
-        return $category->load(['recipes']);
+        $category = $category->load('recipes');
+        return new CategoryResource($category);
     }
 }
